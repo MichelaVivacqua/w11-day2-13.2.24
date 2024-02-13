67d3f6358b2f6f98b8772efe17fd1c72d7860a67
MichelaVivacqua <vivacquamichela@gmail.com>
2024-02-13 16:46:43 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MainSearch from "./components/MainSearch";
+import CompanySearchResults from "./components/CompanySearchResults";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Favourites from "./components/Favourites";
+// import { Spinner } from "react-bootstrap";
+// import { useSelector } from "react-redux";
+
+const App = () => {
+  // const isLoading = useSelector((state) => state.job.isLoading);
+  // console.log(isLoading);
+  return (
+    <BrowserRouter>
+      {/* {isLoading && <Spinner variant="success" animation="border" />} */}
+      <Routes>
+        <Route path="/" element={<MainSearch />} />
+        <Route path="/:companyName" element={<CompanySearchResults />} />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
